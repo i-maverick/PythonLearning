811409e67660a31308d69016e432894c1db981f8
i-maverick <in79@mail.ru>
2017-04-26 12:35:31 +0300

    Update quickSort.py

--- quickSort.py
+++ quickSort.py
@@ -1,10 +1,10 @@
-def qsort(_lst):
+def qsort(lst):
     if not lst:
         return []
-    return qsort([x for x in lst if x < lst[0]])\
-        + [x for x in lst if x == lst[0]]\
-        + qsort([x for x in lst if x > lst[0]])
+    piv = (lst[0] + lst[len(lst) // 2] + lst[-1]) // 3
+    return qsort([x for x in lst if x < piv])\
+        + [x for x in lst if x == piv]\
+        + qsort([x for x in lst if x > piv])
 
 lst = [54, 26, 93, 17, 77, 31, 44, 55, 20, 10, 22, 101, 546, -15, 23, 66, -55, -17, 0, 5, 223]
-qsort(lst)
-print lst
+print qsort(lst)
