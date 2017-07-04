0cf17495cc4ca1e770ed7b063d23d6f702012c06
i-maverick <in79@mail.ru>
2017-07-04 18:51:50 +0300

    Update dictionary.py

--- dictionary.py
+++ dictionary.py
@@ -1,3 +1,4 @@
+# simple version
 def make_dict(keys, values):
     d = dict.fromkeys(keys)
     for i,v in enumerate(d):
@@ -11,5 +12,9 @@ keys = [0, 1, 2, 3, 4, 5]
 values = ['zero', 'one', 'two', 'three']
 print make_dict(keys, values)
 
+# short version
+print {k: values[i] if i < len(values) else None for i, k in enumerate(keys)}
+
+# itertools version
 from itertools import izip_longest
 print {k: v for k, v in izip_longest(keys, values) if k is not None}
