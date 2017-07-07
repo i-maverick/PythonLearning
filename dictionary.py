b0cc5449cc373adc852a38b145e92eb05f47667a
i-maverick <in79@mail.ru>
2017-07-07 18:54:40 +0300

    update

--- dictionary.py
+++ dictionary.py
@@ -1,15 +1,41 @@
-def make_dict(keys, values):
-    d = dict.fromkeys(keys)
-    for i,v in enumerate(d):
-        if i >= len(values):
-            break
-        d[i] = values[i]
-    return d
-
+from itertools import izip, izip_longest
 
 keys = [0, 1, 2, 3, 4, 5]
 values = ['zero', 'one', 'two', 'three']
-print make_dict(keys, values)
 
-from itertools import izip_longest
+d = dict.fromkeys(keys)
+for k, v in izip(keys, values):
+    d[k] = v
+print d
+
 print {k: v for k, v in izip_longest(keys, values) if k is not None}
+
+from itertools import groupby
+
+city_list = [
+    ('Decatur', 'AL'),
+    ('Huntsville', 'AL'),
+    ('Selma', 'AL'),
+    ('Anchorage', 'AK'),
+    ('Nome', 'AK'),
+    ('Flagstaff', 'AZ'),
+    ('Phoenix', 'AZ'),
+    ('Tucson', 'AZ')
+]
+
+def get_state(tpl):
+    return tpl[1]
+
+# city_list.sort(key = lambda x: x[1])
+for k,v in groupby(city_list, get_state):
+    print k, [x[0] for x in v]
+
+lst = [-1, -2, -1, 10, 10, 10, -1, -1, -1, 10, -1, 10]
+
+def group_ints(lst, key=0):
+    return [list(g) for _, g in groupby(lst, lambda x: x < key)]
+
+print group_ints(lst, 5)
+
+for _, g in groupby(lst, lambda x: x < 5):
+    print k, list(g)
